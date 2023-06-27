8a890c179629247691747aa58b697556e798b042
Budnikov Illia <ilyabudkov@gmail.com>
2023-06-27 12:06:46 +0300

    create new project, course of springframeworkguru

---
+++ src/main/java/com/springframework/spring6webappproject/Spring6WebappProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.spring6webappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6WebappProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6WebappProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springframework/spring6webappproject/Spring6WebappProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.spring6webappproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebappProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
